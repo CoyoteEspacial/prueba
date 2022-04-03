b94a33e40b95bd518b50728a89eb8ad3b39599cb
CoyoteEspacial <coyote.espacial@gmail.com>
2022-04-03 00:23:06 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	
+	<title>Reales, Agrupacion de Colaboracion Empresaria</title>
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<script src="js/bootstrap.js"></script>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Antonio:wght@300&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1"><!-- esto debe estar siempre para el responsibe-->
+
+<!--link bootstrap-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<!-- fin link bootstrap-->
+
+<!-- link material design -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@6.5.95/css/materialdesignicons.min.css
+">
+<!-- End Link Material Design -->
+
+<script type="text/javascript" src="js/archivo.js" defer></script>
+<!--<script type="text/javascript" src="js/jquery-3.6.0.min.js"></script> -->
+
+
+
+
+
+
+</head>
+<body>
+	
+	<header id="inicio" >
+<!-- emblema de Reales -->
+		<div id="marca">
+			<img src="img/logo1.png">
+			
+		</div>
+
+<!-- fin de emblema de Reales -->
+
+<!-- inicio de Menu -->
+		<nav id="menu">
+			<ul>
+	<li><a href="#sec1">Inicio</a></li>
+	<li><a href="#sec2">Historia</a></li>
+	<li><a href="#sec3">Socios</a></li>
+	<li><a href="#sec4">Contacto</a></li>
+
+</ul>
+		</nav>
+
+<!-- fin de Menu -->
+	</header>
+	
+	<section id="sec1">
+
+		<!-- In this section I Will put the name of the Company, with Animation -->
+<div id="animacion">
+<p>Reales A.C.E.
+Agrupacion de Colaboracion Empresaria.</p>
+<p>Desde el año 1998 Colaborando con su Empresa.</p>
+
+
+</div>
+
+<div id="contactese">
+	<button type="button" class="btn btn-outline-warning" ><a href="#footer" style="text-decoration: none; color: white" >Nuestros Contactos</a></button>
+
+
+</div>
+	
+
+	</section>
+</div>
+
+	<section id="sec2"> 
+		
+	<!-- Here we have the history of the company -->
+			<h2>Nuestra Historia</h2>
+
+<div id="historia">
+		<p class="palabra" style="color: white;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+
+		<p class="palabra" style="color: white;">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+</p>
+		</div>
+		
+<!-- End of  history of the company -->
+	</section>
+	
+	<section id="sec3">
+		
+		
+		<div id="socios">
+			<!-- Here will have a section "socios" with photos of diferents partners, made it with bootstrap-->
+		<h2 class="titulo-socio">Socios</h2>
+
+		<p class="parrafo" style="color:white;">Algunos socios que nos acompañan en esta aventura</p>
+
+<!-- Image Gallery with modal -->
+		<!-- buttons -->
+		<div class="overlay">
+			<div class="slideshow">
+				<span class="btn_cerrar">&times;</span>
+				<div class="botones adelante">
+					<i class="mdi mdi-arrow-right-circle-outline"></i>
+				</div>
+				<div class="botones atras">
+					<i class="mdi mdi-arrow-left-circle-outline"></i>
+				</div>
+				<img src="" alt="" id="img_slideshow">
+			</div>
+		</div>
+		<!-- End buttons -->
+		<div class="galeria"> 
+		<div class="columna">
+			<img src="img/img1.jpg" alt="" data-img-mostrar="0">
+			<img src="img/img2.jpg" alt="" data-img-mostrar="1">
+			<img src="img/img3.jpg" alt="" data-img-mostrar="2">
+			<img src="img/img4.jpg" alt="" data-img-mostrar="3">
+			<img src="img/img5.jpg" alt="" data-img-mostrar="4">
+
+		</div>
+		<div class="columna">
+			<img src="img/img6.jpg" alt="" data-img-mostrar="5">
+			<img src="img/img7.jpg" alt="" data-img-mostrar="6">
+			<img src="img/img8.jpg" alt="" data-img-mostrar="7">
+			<img src="img/img9.jpg" alt="" data-img-mostrar="8">
+			<img src="img/img10.jpg" alt="" data-img-mostrar="9">
+		</div>
+		<div class="columna">
+			<img src="img/img11.jpg" alt="" data-img-mostrar="10">
+			<img src="img/img12.jpg" alt="" data-img-mostrar="11">
+			<img src="img/img13.jpg" alt="" data-img-mostrar="12">
+			<img src="img/img14.jpg" alt="" data-img-mostrar="13">
+			<img src="img/img15.jpg" alt="" data-img-mostrar="14">
+		</div>
+		<div class="columna">
+			<img src="img/img16.jpg" alt="" data-img-mostrar="15">
+			<img src="img/img17.jpg" alt="" data-img-mostrar="16">
+			<img src="img/img18.jpg" alt="" data-img-mostrar="17">
+			<img src="img/img19.jpg" alt="" data-img-mostrar="18">
+			<img src="img/img20.jpg" alt="" data-img-mostrar="19">
+		</div>
+		<div class="columna">
+			<img src="img/img21.jpg" alt="" data-img-mostrar="20">
+			<img src="img/img22.jpg" alt="" data-img-mostrar="21">
+			<img src="img/img23.jpg" alt="" data-img-mostrar="22">
+			<img src="img/img24.jpg" alt="" data-img-mostrar="23">
+			<img src="img/img25.jpg" alt="" data-img-mostrar="24">
+		</div>
+        </div>
+		
+
+<!-- End Image Gallery with modal -->
+
+
+		<!-- End section "Socios"-->
+
+		</div>
+		
+		<div id="noticias">
+			<h2 class="titulo-socio">Noticias</h2>
+			<ul id="ul">
+				<li><a href="#" target="new" style="text-decoration: none; color: white;">Le damos la bienvenida a un nuevo Socio: Supmercados "Constancio".</a></li>
+
+				<li><a href="#" target="new" style="text-decoration: none; color: white;">tenemos el agrado de informarles que Galletas de Agua, se suma a nuestros productos.</a></li>
+
+			
+</ul>
+
+
+		</div>
+
+
+		
+		<div id="marcas">
+
+			<h2 id="empresas">Algunas de las empresas que nos confian sus productos</h2>
+
+			<!--  "marcas" section  of diferent branches, made it with javascript -->
+                  
+                  <div id='div1'>
+                          <div id='div2'>
+                              <ul>
+                           <li id="li"><a href="https://www.unilever.com.ar/" id="a" target="new"><img src="img/unilever.png"  id="img" style="height:100px"></a></li>
+                           <li id="li"><a href="https://www.molinocanuelas.com/" id="a" target="new"><img src="img/cañuelas.jpg"  id="img" style="height:100px" ></a></li>
+                           <li id="li"><a href="https://www.grupopenaflor.com.ar/" id="a" target="new"><img src="img/peñaflor.jpg" id="img" style="height:100px" ></a></li> 
+                           <li id="li"><a href="https://www.baggio.com.ar/" id="a" target="new"><img src="img/RPB.jpg" id="img" style="height:100px"></a></li> 
+                           <li id="li"><a href="https://www.lasmarias.com.ar/" id="a" target="new"><img src="img/LasMarias.jpg" id="img" style="height:100px"></a></li>
+                           <li id="li"><a href="https://www.alicorp.com.pe/ar/es/productos/?sector=Consumo%20masivo" id="a" target="new"><img src="img/alicorp.jpg" id="img" style="height:100px"></a></li>
+                           <li id="li"><a href="https://latam.pg.com/pg-argentina/" id="a" target="new"><img src="img/pg.jpg" id="img" style="height:100px"></a></li>
+                           <li id="li"><a href="https://www.molinos.com.ar/" id="a" target="new"><img src="img/molinos.jpg" id="img" style="height:100px"></a></li>
+                             </ul>
+                        </div>
+                    </div>                   
+
+
+
+
+
+			<!-- End of Marcas section of diferent branches, made it with javascript -->
+	
+
+
+</div> 
+
+		</div>
+	
+	</section>
+	<section id="sec4">
+		
+		<p class="contacto" style="color:white;">Contactese con nosotros</p>
+			
+	    <div id="contacto">
+			
+			<!-- section "Contactos" made it with bootstrap -->
+
+			<form>
+	<input class="form-control" type="text" name="nombre"  placeholder="Escriba su Nombre y Apellido" maxlength="50" required><br>
+	
+	<input class="form-control" type="email" name="mail"  placeholder="Escriba su Correo" maxlength="30" required><br>
+	<input class="form-control" type="text" name="text"  placeholder="Asunto" maxlength="30" required><br>
+	<textarea class="form-control" rows="3" maxlength="200" name="consulta" requiered placeholder="Consulta"></textarea>
+	<input type="submit" Value="Enviar" class="btn btn-warning">
+</form>
+
+
+
+<!-- fin bootstrap -->
+		<!-- flecha -->
+         <div id="flecha">
+		<a href="#sec1" style="color: white; "><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+</svg></a>
+
+</div>
+		<!-- End Flecha -->
+
+		</div>
+	</section>
+    <footer id="footer">
+
+    	<!-- Here is part of telefono section-->
+    	<div id="telefono">
+    	<div id="logo-telefono">	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="q" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+</svg></div>
+
+
+
+
+<p class="tel">Nuestros Telefonos:</p>
+<p class="tel1">Tel: (0221) 4916558 / 49165559
+</p>
+
+    	</div>
+
+    <!-- End of Telefno Section -->
+
+    <!-- Here is part of direccion section-->
+
+    	<div id="direccion">
+    		
+    	<div id="logo-direccion">	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+</svg>
+</div>
+
+<p class="dir">Nuestra direccion:</p>
+<p class="dir1">208 y 532.</p>
+    	</div>
+<!-- End of Direccion section-->
+
+<!-- Here is part of Email section-->
+    	<div id="email"> 
+    	<div id="logo-email">	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+</svg></div>
+
+<p class="mail1">Escribanos al E-Mail:</p>
+<p class="mail">administracion@agrupacionreales.com.ar</p>
+<p class="mail2">ventas@agrupacionreales.com.ar</p>
+    	</div>
+
+
+  <!-- End of Direccion section-->  	
+    </footer>
+</body>
+</html>
